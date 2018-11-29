b35967a31c82d426be96bb9df1ba2b309800b2ba
SteffanyNg <sxng@uw.edu>
2018-11-29 15:03:26 -0800

    added navigation tabs and comments to ui.R

--- college_information/ui.R
+++ college_information/ui.R
@@ -1,33 +1,87 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
+
+  # We can remove the separators after we are done
+  # Plots don't show if you run the app rn 
+  # because it's the same code & plot for each tab
+  # so don't panic! (just letting you know if you wonder why)
   
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+  navbarPage(
+    title = "College Information",
+    tabPanel("About", "This is the about page."),
+
+    # =========== Paste your code after the text ===========
+
+    tabPanel(
+      "Locations", # feel free to change the title!
+      "You can change the text here to whatever you need",
+
+      # This is just an example, delete this and put your code here with your code.
+      sidebarLayout(
+        sidebarPanel(
+          sliderInput("bins",
+            "Number of bins:",
+            min = 1,
+            max = 50,
+            value = 30
+          )
+        ),
+
+        mainPanel(
+          plotOutput("distPlot")
+        )
+      )
     ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
+    # =========== Paste your code after the text ===========
+
+    tabPanel(
+      "Population", # feel free to change the title!
+      "You can change the text here to whatever you need",
+
+      # This is just an example, delete this and put your code here with your code.
+      sidebarLayout(
+        sidebarPanel(
+          sliderInput("bins",
+            "Number of bins:",
+            min = 1,
+            max = 50,
+            value = 30
+          )
+        ),
+
+        mainPanel(
+          plotOutput("distPlot")
+        )
+      )
+    ),
+    # =========== Paste your code after the text ===========
+
+    tabPanel(
+      "Scores", # feel free to change the title!
+      "You can change the text here to whatever you need",
+      # This is just an example, delete this and put your code here with your code.
+      sidebarLayout(
+        sidebarPanel(
+          sliderInput("bins",
+            "Number of bins:",
+            min = 1,
+            max = 50,
+            value = 30
+          )
+        ),
+
+        mainPanel(
+          plotOutput("distPlot")
+        )
+      )
+    ),
+    # =========== Paste your code after the text ===========
+
+    tabPanel(
+      "Admission Rates",
+      "You can change the text here to whatever you need"
     )
+    # ======================================================
   )
 ))
