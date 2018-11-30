eb5d57826db6285f9ce7bf1f39983f0560d3c829
johanlia000 <lxjohansen@gmail.com>
2018-11-29 17:34:17 -0800

    updated the readme file

--- college_information/ui.R
+++ college_information/ui.R
@@ -60,16 +60,23 @@ shinyUI(fluidPage(
 
     tabPanel(
       "Scores", # feel free to change the title!
-      "You can change the text here to whatever you need",
+      "LIA THIS IS YOURS",
       # This is just an example, delete this and put your code here with your code.
       sidebarLayout(
         sidebarPanel(
-          sliderInput("bins",
-            "Number of bins:",
-            min = 1,
-            max = 50,
-            value = 30
-          )
+          # Select a particular state
+          selectInput("State", label = h3("Select a State"), 
+                      choices = list("Alabama"="AL", "Alaska"="AK", "Arizona"="AZ", "Arkansas"="AR",
+                                     "California"="CA", "Colorado"="CO", "Connecticut"="CT", "Delaware"="DE",
+                                     "Florida"="FL", "Georgia"="GA", "Hawaii"="HI", "Idaho"="ID", "Illinois"="IL",
+                                     "Indiana"="IN", "Iowa"="IA", "Kansas"="KS", "Kentucky"="KY", "Louisiana"="LA",
+                                     "Maine"="ME", "Maryland"="MD", "Massachusetts"="MA", "Michigan"="MI", "Minnesota"="MN",
+                                     "Mississippi"="MS", "Missouri"="MO", "Montana"="MT", "Nebraska"="NE", "Nevada"="NV",
+                                     "New Hampshire"="NH", "New Jersey"="NJ", "New Mexico"="NM", "New York"="NY", "North Carolina"= "NC",
+                                     "North Dakota"="ND", "Ohio"="OH", "Oklahoma"="OK", "Oregon"="OR", "Pennsylvania"="PA",
+                                     "Rhode Island"="RI", "South Carolina"="SC", "South Dakota"="SD", "Tennessee"="TN",
+                                     "Texas"="TX", "Utah"="UT", "Vermont"="VT", "Virginia"="VA", "Washington"="WA", "West Virginia"="WV",
+                                     "Wisconsin"="WI","Wyoming"="WY"), selected = "AL")
         ),
 
         mainPanel(
