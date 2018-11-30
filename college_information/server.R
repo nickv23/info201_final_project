929f0e6d0cedad382c537dd6878598379f85751e
johanlia000 <lxjohansen@gmail.com>
2018-11-29 17:21:32 -0800

    added in sections for eahc person's code in the server file

--- college_information/server.R
+++ college_information/server.R
@@ -1,11 +1,36 @@
 library(shiny)
+library(ggplot2)
+library(scales)
+library(dplyr)
+library(leaflet)
 
 college_data <- read.csv("MERGED2016_17_PP.csv", stringsAsFactors = FALSE)
 
 shinyServer(function(input, output) {
    
+  # =========== Paste your code after the text ===========
+  # Nick!!!!!!
+  # ======================================================
   
+  
+  
+  # =========== Paste your code after the text ===========
+  # Linley!!!
+  # ======================================================
+  
+  
+  # =========== Paste your code after the text ===========
+  # Lia!!!
+  # ======================================================
+  
+  
+  
+  # =========== Paste your code after the text ===========
+  # Steffany!!!
+  # ======================================================
 
+  
+  
   
   output$distPlot <- renderPlot({
     
